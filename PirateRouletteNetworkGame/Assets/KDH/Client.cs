8cb87722ce176a71622d962a00138ae951f6a5db
watertree34 <40683453+watertree34@users.noreply.github.com>
2020-12-20 16:44:05 +0900

    client_final

---
+++ PirateRouletteNetworkGame/Assets/KDH/Client.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+public class Client : MonoBehaviour
+{
+    public string ClientName;
+
+    public int clientID;
+    public Text label_ClientID;
+
+    private bool socketReady;
+    private TcpClient socket;
+    private NetworkStream stream;
+
+    private BinaryWriter bWriter;
+    private BinaryReader bReader;
+
+    private bool myTurn = true;
+    float camPoint;
+
+    public CollisionColumnScript ccs;
+
+    public GameObject inputField;
+
+    public HoleScript[] hs;
+
+    public string host = "127.0.0.1";
+    public int port = 6321;
+
+    public int turn;
+
+    public Text labelTurn;
+
+   
+
+    void Awake()
+    {
+        Screen.SetResolution(640,960, FullScreenMode.Windowed);
+    }
+
+    private void Start()
+    {
+        clientID = -1;
+      
+    }
+
+    public void SetClientName(string param)
+    {
+        ClientName = param;
+    }
+
+    public void SetHost(string param)
+    {
+        host = param;
+    }
+
+    public void SetPort(string param)
+    {
+        port = int.Parse(param);
+    }
+
+    public void ConnectedToServer()
+    {
+        //if already connected, ignore this function
+        if (socketReady)
+            return;
+        // Default host / port 
+        string host = this.host;
+        int port = this.port;
+
+        //create the socket
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+
+            bWriter = new BinaryWriter(stream);
+            bReader = new BinaryReader(stream);
+            socketReady = true;
+
+            inputField.SetActive(false);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error : " + e.Message);
+        }
+    }
+
+    public void SetCamPointValue(float value)
+    {
+        camPoint = value;
+        if (myTurn)
+        {
+          
+            if (socketReady)
+            {
+                {
+                    if (clientID >= 0)
+                    {
+                        bWriter.Write((int)MessageID.MOVE);
+                        bWriter.Write(clientID);
+                        bWriter.Write(camPoint);
+                    }
+                }
+
+                if (stream.DataAvailable)
+                {
+                    OnIncomingData();
+                }
+            }
+        } 
+       
+    }
+
+    public void PutKnife(int idx)
+    {
+        if (myTurn)
+        {
+            if (socketReady)
+            {
+                {
+                    if (clientID >= 0)
+                    {
+                        bWriter.Write((int) MessageID.KNIFE);
+                        bWriter.Write(clientID);
+                        bWriter.Write(idx);
+                    }
+                }
+
+                if (stream.DataAvailable)
+                {
+                    OnIncomingData();
+                }
+            }
+        }
+    }
+
+    public void TurnPass()
+    {
+        if (clientID != turn)
+        {
+            return;
+        }
+        
+        if (myTurn)
+        {
+            if (socketReady)
+            {
+                {
+                    if (clientID >= 0)
+                    {
+                        bWriter.Write((int) MessageID.TURNPASS);
+                        bWriter.Write(clientID);
+                    }
+                }
+
+                if (stream.DataAvailable)
+                {
+                    OnIncomingData();
+                }
+            }
+        }
+    }
+
+    public void Killed()
+    {
+        if (myTurn)
+        {
+            if (socketReady)
+            {
+                {
+                    if (clientID >= 0)
+                    {
+                        bWriter.Write((int) MessageID.KILLED);
+                        bWriter.Write(clientID);
+                    }
+                }
+
+                if (stream.DataAvailable)
+                {
+                    OnIncomingData();
+                }
+            }
+        }
+    }
+
+    // 매 프레임 업데이트 절차 거침
+    private void Update()
+    {
+        if (socketReady)
+        {
+            if (stream.DataAvailable)
+                OnIncomingData();
+        }
+    }
+ 
+    private void OnIncomingData()
+    {
+        int messageID = bReader.ReadInt32();
+
+        Debug.Log((bReader));
+        int id;
+        byte[] buffer;
+
+        switch (messageID)
+        {
+            case (int) MessageID.INIT:
+                Debug.Log("init");
+                id = bReader.ReadInt32();
+                clientID = id;
+                label_ClientID.text = "내 차례: " + (clientID+1).ToString();
+                
+                buffer = Message.getBytes(MessageID.NEW, id, camPoint);
+                Send(buffer);
+                break;
+
+            case (int) MessageID.NEW:
+                Debug.Log("new");
+                id = bReader.ReadInt32();
+                int rand = bReader.ReadInt32();
+                ccs.randomNum = rand;
+                break;
+
+            case (int) MessageID.MOVE:
+                Debug.Log("move");
+                id = bReader.ReadInt32();
+                camPoint = bReader.ReadSingle();
+                Camera.main.transform.parent.eulerAngles = Vector3.up * camPoint;
+                break;
+            case (int) MessageID.KNIFE:
+                Debug.Log("KNIFE");
+                id = bReader.ReadInt32();
+                int putNum = bReader.ReadInt32();
+                hs[putNum].SpawnKnife();
+                break;
+            case (int) MessageID.KILLED:
+                Debug.Log("KILLED");
+                id = bReader.ReadInt32();
+                ccs.Finish();
+                break;
+            case (int) MessageID.TURNPASS:
+                Debug.Log("TurnPass");
+                id = bReader.ReadInt32();
+                turn = bReader.ReadInt32();
+                labelTurn.text = (turn + 1).ToString();
+                
+                StartCoroutine(ccs.Pass(turn));
+                break;
+            default:
+                Debug.Log(" default in switch. error!!" + messageID);
+                break;
+        }
+    }
+
+    private void Send(byte[] data)
+    {
+        if (!socketReady)
+            return;
+        bWriter.Write(data, 0, 20);
+        Debug.Log("sent data: " + data);
+    }
+
+    public void OnSendButton()
+    {
+        //string message = GameObject.Find("SendInput").GetComponent<InputField>().text;
+        //Send(message);
+    }
+
+    private void CloseSocket()
+    {
+        if (!socketReady)
+            return;
+        bWriter.Close();
+        bReader.Close();
+        socket.Close();
+        socketReady = false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseSocket();
+    }
+
+    private void OnDisable()
+    {
+        CloseSocket();
+    }
+}
---
+++ PirateRouletteNetworkGame/Assets/NHY/Scripts/CollisionColumnScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionColumnScript : MonoBehaviour
+{
+    int nowNum = 0;
+    public int personNum = -1;
+    
+    public int randomNum = 0;
+    public GameObject person;
+    Rigidbody rb;
+    public GameObject gameOver;
+    public GameObject pass;
+
+    public Image edge;
+    public Image noActive;
+
+    public Client cl;
+
+    private GameObject nowKnife;
+
+  
+
+    private void Start()
+    {
+        randomNum = Random.Range(1, 18);   // 랜덤 숫자 정하기
+        rb = person.gameObject.GetComponent<Rigidbody>();  //해적의 리지드바디
+        gameOver.SetActive(false);
+        pass.SetActive(false);
+        edge.enabled = false;
+        noActive.enabled = false;
+    }
+    private void Update()
+    {
+        if (personNum != cl.clientID)
+        {
+            edge.enabled = false;
+            noActive.enabled = true;
+        }
+        else
+        {
+            edge.enabled = true;
+            noActive.enabled = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Knife")  // 칼이 들어올때
+        {
+            KnifePlus(other.gameObject);
+        }
+    }
+
+    public void KnifePlus(GameObject nowKnife)  // 칼 추가 함수
+    {
+        this.nowKnife = nowKnife;
+        nowKnife.GetComponent<KnifeScript>().enabled = false;  // 움직임 코드를 멈추고
+        nowNum++;  //숫자 증가
+        ActiveScript.Instance.active = true;  // 칼이 다 들어가면 다른거 선택가능
+
+        if (nowNum == randomNum)   // 만약 현재 숫자가 랜덤숫자면 
+        {
+        
+            ActiveScript.Instance.active = false;  // 끝나면 다른거 선택불가
+            cl.Killed();
+        }
+        else if(cl.turn == personNum)
+        {
+     
+            cl.TurnPass();
+        }
+    }
+
+    public IEnumerator Pass(int turn)  //통과 효과
+    {
+        personNum = turn;
+        pass.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        pass.SetActive(false);
+    }
+
+    public void Finish()  // 종료 효과
+    {
+        // 해적 튀어나가기
+        nowKnife.GetComponent<Renderer>().material.color = Color.red;  // 칼 색깔 빨강으로 바뀜
+
+        rb.AddForce(Vector3.up * 100 + (-Vector3.right * 50));
+
+        gameOver.SetActive(true);
+
+        StartCoroutine(CoFinish());
+    }
+
+    IEnumerator CoFinish()
+    {
+        yield return new WaitForSeconds(1f);
+        
+        Application.Quit();
+    }
+}
